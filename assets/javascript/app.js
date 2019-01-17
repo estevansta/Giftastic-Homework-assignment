1ef3ecfcff6c83af2bce0520c25aebd6e0ba4770
Estevan Cruz <Estevancruz@Estevans-MacBook-Pro.local>
2019-01-17 15:03:20 -0600

    trouble shooting why data-name isnt appearing in the dynamic button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,26 +8,29 @@
 var characters = ["Spongebob Squarepants", "Patrick Star", "Squidward Tentacles", "Sandy Cheeks"];
 console.log(characters);
 
+// data attribute capturing function/ code source: 6.2 ajax 08 -->
+    var characterName = $(this).attr("data-name");
+
 // create a function to display character names from the global variable "characters"
 // function renderButtons 
 function renderButtons(){
+    $(".character-buttons").empty();
     // for loop to loop through the character array
     for (var i=0; i < characters.length; i++); {
         //using the for loop we can dynamically generate the buttons that loop through the character array
         //using jquery to generate the <button> tag in the character-button class
         var character = $("<button>");
-        // add class to the button
+        // add class to the button "character"
         character.addClass("character");
-        // add data-attribute which is the name of each character
+        // add data-attribute which is the name of each character "spongebob squarepants"
         character.attr("data-name", characters[i]);
-        // text for the buttons
+        // text for the buttons "<button>spongebob squarepants</button>""
         character.text(characters[i]);
         // add the buttons to html by appending with $ .class to 
         $(".character-buttons").append(character);
-        console.log(characters.length);
-        console.log(character);
+
     }
-}
+};
 
 // calling the renderButtons function to display buttons
 renderButtons();
