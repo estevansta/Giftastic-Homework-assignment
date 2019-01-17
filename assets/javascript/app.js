80301d4a6b0f9f8267a2ef9457223161af531031
Estevan Cruz <Estevancruz@Estevans-MacBook-Pro.local>
2019-01-17 13:36:13 -0600

    Initial pseudocode, linked html/js/css

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+// create a webpage that has a theme of buttons that when clicked:
+// loads 10 gifs
+// when gifs are clicked they move; and stop when clicked again.
+// option to add button to the already populated array (looping) 
